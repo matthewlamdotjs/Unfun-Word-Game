5b59340b4e225a590b0c6dc12bcaa589c37a8d40
mattlammmmm <matthew.lam.93@icloud.com>
2015-04-04 17:14:35 -1000

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+Matthew Lam
+Pengo: mlam
+Email: matthew.lam.93@icloud.com
+Filename: main.cpp
+Assignment 4 - Unfun
+Description: main function runs the program
+Code status:
+*/
+
+#include "Unfun.h"
+
+
+int main(){
+	Unfun *un = new Unfun(); 
+	un->play();
+
+	return 0;
+}
